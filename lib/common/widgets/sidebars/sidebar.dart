a41032ed9b1e112641eb1259a283267d1a48804a
AnshulVThomas <anshulvthomas@gmail.com>
2025-07-06 15:24:37 +0530

    fixed logo space issue

--- lib/common/widgets/sidebars/sidebar.dart
+++ lib/common/widgets/sidebars/sidebar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:wevn/common/widgets/images/circluar_svg_image.dart';
 import 'package:wevn/common/widgets/images/circular_png_image.dart';
 import 'package:wevn/common/widgets/layouts/sizes/page_sizes.dart';
 import 'package:wevn/common/widgets/menu_items/hoverable_expanded_tile.dart';
@@ -28,17 +29,19 @@ class TSidebar extends StatelessWidget {
 
         child: SingleChildScrollView(
           child:Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              TCircularPngImage(imagePath: "assets/icons/logo1.png", size: 100, fit: BoxFit.cover, backgroundColor: Colors.white, borderColor: Colors.black12, borderWidth: 1.0,padding: TSizes.md,),
-
-              SizedBox(height: TSizes.spaceBetweenItems,),
-              Padding(padding: const EdgeInsets.all(TSizes.md),
+              // TCircularPngImage(imagePath: "assets/icons/logo2.png", size: 100, fit: BoxFit.cover, backgroundColor: Colors.white, borderColor: Colors.black12, borderWidth: 1.0,padding: TSizes.md,),
+                TCircularSvgImage(svgAsset: "assets/icons/logo_dark.svg", size: 100, fit: BoxFit.cover, backgroundColor: Colors.white, borderColor: Colors.black12,padding: TSizes.md,),
+             
+              Padding(padding: const EdgeInsets.all(TSizes.xs),
                 child: 
                 
                 Column(
                   children: [
                     Text("WevN", style: Theme.of(context).textTheme.bodyLarge!.apply(letterSpacingDelta: 1.2),),
 
+                    const SizedBox(height: TSizes.sm,),
                     TMenuItem(route: TRoutes.dashboard, icon: Iconsax.status, itemName: "Dashboard",),
                     TMenuItem(route: TRoutes.nodes, icon: Iconsax.status, itemName: "Nodes",),
                     TMenuItem(route: TRoutes.graph, icon: Iconsax.status, itemName: "Graph",),
